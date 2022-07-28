e8927a7b2ad725ba828e170a8ab18f3b4e1ef2b2
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2022-07-28 17:59:25 +0900

    enigma

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from enigma import ALPHABET, Reflector
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    r = Reflector('BACD')
+    i = r.reflect(ALPHABET.index('A'))
+    print(ALPHABET[i])
+    i = r.reflect(ALPHABET.index('B'))
+    print(ALPHABET[i])
+    i = r.reflect(ALPHABET.index('C'))
+    print(ALPHABET[i])
+    i = r.reflect(ALPHABET.index('D'))
+    print(ALPHABET[i])
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
